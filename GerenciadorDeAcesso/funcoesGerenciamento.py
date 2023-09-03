9d2a7aa2a6b2be5a03d07852fa6dd72611fae51b
Edcarlos <85942078+Levi-cell@users.noreply.github.com>
2023-09-03 16:40:36 -0300

    Update funcoesGerenciamento.py

--- GerenciadorDeAcesso/funcoesGerenciamento.py
+++ GerenciadorDeAcesso/funcoesGerenciamento.py
@@ -44,8 +44,8 @@ def entrada():
             cursor.execute("""INSERT INTO registro_entrada (data_hora_entrada, codigo_funcionario)
                                               VALUES(?, ?)""", (horaInicio, resultado[0],))
             conexao.commit()
-            print(f"""Olá {resultado[1]} seja bem vindo a operação, a contagem do seu tempo
-            de trabalho começa agora, data e hora de ínicio: {horaInicio}  """ + "\n")
+            print(f"""Olá {resultado[1]} seja bem vindo a operação, a contagem do seu tempo de trabalho começa agora...""")
+            print(f"""data e hora de ínicio: {horaInicio}""" + "\n")
 
     else:
         print("Você não está no nosso banco de dados." + "\n")
